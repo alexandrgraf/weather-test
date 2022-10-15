0c216d456f30a5bc1c105ecd8e8dae31e9f936c7
atyshkevich <atyshkevich@eisgroup.com>
2022-10-15 12:22:18 +0300

    fix services file

--- src/services/weatherAPI/index.ts
+++ src/services/weatherAPI/index.ts
@@ -1,8 +1,8 @@
-const API_KEY = "0ae63dec94f54a2aac4104407221210"
+const API_KEY = "234039565b4d4983ab4105335221210"
 
 export const getWeather = (cityCode: string, days: number = 4) =>
-    fetch(`http://api.weatherapi.com/v1/forecast.json?key=234039565b4d4983ab4105335221210&q=${cityCode}&days=${days}`)
+    fetch(`http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${cityCode}&days=${days}`)
         .then(response => response.json())
         .catch(err => {
-            console.error('Some error:', err);
-        });
+            console.error('API error:', err);
+        })
